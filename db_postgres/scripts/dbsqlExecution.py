232eb49148606d2821952ef4d9767fd470380fa3
Kar, Prangya Parmita <Prangya.Kar@bhge.com>
2018-01-25 16:22:16 -0800

    Added new file for sql execution dbsqlExecution.py

---
+++ db_postgres/scripts/dbsqlExecution.py
@@ -0,0 +1,122 @@
+#################################################
+#
+# Purpose: This script will extract dynamically the db credentials for source and destinations databases.
+#
+#
+# -----------    ----------------  -------------------------------------
+# Date           Author            Comment
+# -----------    ----------------  -------------------------------------
+# Jan,25.2018    Prangya P Kar      Intial Version
+#
+##################################################
+from itertools import islice # to exclude the first n last line used islice
+def source_db():
+
+#print ("opening n closing of file Source")
+
+    source_file=open("source.txt","r")
+    d={}
+
+    #for line in islice(source_file,4,14):
+    #for line in islice(source_file,1,12):
+    for line in source_file:
+        if ":" in line:
+            x=line.split(":",1)
+            a=x[0]
+            a=a.strip()
+            b=x[1]
+            b=x[1].replace(',', '')
+            #c=len(b)-2 #The -1 get len of b -'\n'
+            #b=b[0:c]
+            b=b.strip()
+            d[a]=b
+
+#    print ("\nSOURCE FILE INFO ....")
+#     print(d)
+
+    #print ("\nSOURCE FILE INFO ....")
+
+#    print ("\nGENERATING SOURCE FILE INFO ... in db_credentials.txt")
+
+    with open('db_commands.txt', 'w') as f:
+
+        if '"host"' in d:
+            SRC_DB_HOST=(d['"host"'])
+            print >> f, "export SRC_DB_HOST="+SRC_DB_HOST
+        elif '"hostname"' in d:
+            SRC_DB_HOST = (d['"hostname"'])
+            print >> f, "export SRC_DB_HOST=" +SRC_DB_HOST
+
+        SRC_DB_PORT=(d['"port"'])
+        print >>f, "export SRC_DB_PORT="+SRC_DB_PORT
+
+        SRC_DB_NAME=(d['"database"'])
+        print >>f, "export SRC_DB_NAME="+SRC_DB_NAME
+
+        SRC_USER_NAME=(d['"username"'])
+        print >>f, "export SRC_USER_NAME="+SRC_USER_NAME
+
+        SRC_USER_PASSWORD=(d['"password"'])
+        print >>f, "export SRC_USER_PASSWORD="+SRC_USER_PASSWORD
+
+    source_file.close()
+
+##################
+
+def dest_db():
+#print ("\nopening n closing of file Destination")
+#from itertools import islice
+
+    destination_file = open("destination.txt", "r")
+    d = {}
+    #for line in islice(destination_file, 4, 13):
+    #for line in islice(destination_file, 1, 12):
+    for line in destination_file:
+        if ":" in line:
+            x = line.split(":", 1)
+            a = x[0]
+            a = a.strip()
+            b = x[1]
+            b=x[1].replace(',', '')
+            #c = len(b) - 2  # The -1 get len of b -'\n'
+            #b = b[0:c]
+            b = b.strip()
+            d[a] = b
+
+
+#    print ("\nDESTINATION FILE INFO....")
+#     print(d)
+
+    #print ("\nDESTINATION FILE INFO....")
+#    print ("\nGENERATING DESTINATION FILE INFO .. in db_credentials.txt")
+
+    with open('db_commands.txt', 'a') as f:
+
+        if '"host"' in d:
+            DEST_DB_HOST=(d['"host"'])
+            print >> f, "export DEST_DB_HOST="+DEST_DB_HOST
+        elif '"hostname"' in d:
+            DEST_DB_HOST = (d['"hostname"'])
+            print >> f, "export DEST_DB_HOST=" +DEST_DB_HOST
+
+        DEST_DB_NAME = (d['"database"'])
+        print >>f, "export DEST_DB_NAME=" + DEST_DB_NAME
+
+        DEST_DB_PORT = (d['"port"'])
+        print >>f,"export DEST_DB_PORT=" + DEST_DB_PORT
+
+        DEST_USER_NAME = (d['"username"'])
+        print >>f,"export DEST_USER_NAME=" + DEST_USER_NAME
+
+        DEST_USER_PASSWORD = (d['"password"'])
+        print >>f, "export DEST_USER_PASSWORD=" + DEST_USER_PASSWORD
+
+    destination_file.close()
+
+
+def main():
+    source_db()
+    #dest_db()
+
+if __name__ == "__main__":
+        main()

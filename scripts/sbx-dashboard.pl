30b34b3aa5e1308ca01463fd166dd066d9ee02e5
Farooqi, Sufyan <Sufyan.Farooqi@ge.com>
2018-02-22 12:22:17 -0800

    Removed DEV01 and QA01. Added demo environments.

--- scripts/sbx-dashboard.pl
+++ scripts/sbx-dashboard.pl
@@ -9,15 +9,15 @@ chomp ($time_stamp);
 my $cf_user = $ARGV[0];
 my $cf_pwd = $ARGV[1];
 
-my $dev01_sbx_space_id="3e35f208-241f-4cc3-b0e7-025ece212864";
 my $dev02_sbx_space_id="8af7f485-d7b5-4c65-b543-20926f07f774";
-my $qa01_sbx_space_id="99816a5b-b6b4-46c2-83c6-c4ffcc544bab";
 my $qa02_sbx_space_id="357b9319-3942-41ed-8efb-7aa257efa05e";
+my $dd02_sbx_space_id="81028cf1-b5f7-43af-8643-8ff3b24ee557";
+my $dp02_sbx_space_id="ea931220-1af1-4ae7-bb7b-d55cabcc4d7c";
 
-my %dev01_hash;
 my %dev02_hash;
-my %qa01_hash;
 my %qa02_hash;
+my %dd02_hash;
+my %dp02_hash;
 
 my $hash_ref;
 my $app_name;
@@ -27,17 +27,19 @@ my $report_name = 'sbx_dashboard.html';
 
 `cf login -a https://api.system.aws-usw02-pr.ice.predix.io -u $cf_user -p $cf_pwd -o OGD_Development_USWest_01 -s dev02`;
 
-$hash_ref = create_hash($dev01_sbx_space_id);
-%dev01_hash = %$hash_ref;
-
 $hash_ref = create_hash($dev02_sbx_space_id);
 %dev02_hash = %$hash_ref;
 
-$hash_ref = create_hash($qa01_sbx_space_id);
-%qa01_hash = %$hash_ref;
-
 $hash_ref = create_hash($qa02_sbx_space_id);
 %qa02_hash = %$hash_ref;
+
+`cf login -a https://api.system.aws-usw02-pr.ice.predix.io -u $cf_user -p $cf_pwd -o 'Oil&Gas_Product_Demo' -s demodev02`;
+
+$hash_ref = create_hash($dd02_sbx_space_id);
+%dd02_hash = %$hash_ref;
+
+$hash_ref = create_hash($dp02_sbx_space_id);
+%dp02_hash = %$hash_ref;
 
 # Create HTML Report below
 
@@ -46,21 +48,21 @@ open (my $fh, '>', $report_name) or die "Could not create file.\n";
 print $fh "<html lang=\"en\" xml:lang=\"en\" xmlns= \"http://www.w3.org/1999/xhtml\"><title>Sandbox apps dashboard</title>\n<body>\n";
 print $fh "<table border=\"1\">\n";
 print $fh "<tr bgcolor=\"#30aaf4\"><th colspan=\"100%\" align=\"left\"><font size=\"5\">Sandbox environments dashboard</font> - Last run on $time_stamp PST<BR></th></tr>\n";
-print $fh "<tr bgcolor=\"#30aaf4\">\n<th NOWRAP>Sr. No.</th><th>Application Name</th><th>DEV01</th><th>DEV02</th><th>QA01</th><th>QA02</th></tr>\n";
+print $fh "<tr bgcolor=\"#30aaf4\">\n<th NOWRAP>Sr. No.</th><th>Application Name</th><th>DEV02</th><th>QA02</th><th>DEMODEV02</th><th>DEMOPROD02</th></tr>\n";
 
-@dev_apps = sort keys %dev01_hash;
+@dev_apps = sort keys %dev02_hash;
 for $app_name (@dev_apps)
 {
-    my $dev01_app = $dev01_hash{$app_name}[6];
     my $dev02_app = $dev02_hash{$app_name}[6];
-    my $qa01_app = $qa01_hash{$app_name}[6];
     my $qa02_app = $qa02_hash{$app_name}[6];
+    my $dd02_app = $dd02_hash{$app_name}[6];
+    my $dp02_app = $dp02_hash{$app_name}[6];
 
     print $fh "<tr BGCOLOR=\"#e2f4ff\"><td NOWRAP bgcolor=\"#30aaf4\">$app_count</td><td NOWRAP bgcolor=\"#30aaf4\">$app_name</td>";
-    print $fh "<td NOWRAP BGCOLOR=\"$dev01_hash{$app_name}[5]\">$dev01_app <BR>Version: 1.0.0.$dev01_hash{$app_name}[0]<BR>Instances running: $dev01_hash{$app_name}[3]/$dev01_hash{$app_name}[4]<BR>Route: $dev01_hash{$app_name}[2]</td>";
     print $fh "<td NOWRAP BGCOLOR=\"$dev02_hash{$app_name}[5]\">$dev02_app <BR>Version: 1.0.0.$dev02_hash{$app_name}[0]<BR>Instances running: $dev02_hash{$app_name}[3]/$dev02_hash{$app_name}[4]<BR>Route: $dev02_hash{$app_name}[2]</td>";
-    print $fh "<td NOWRAP BGCOLOR=\"$qa01_hash{$app_name}[5]\">$qa01_app <BR>Version: 1.0.0.$qa01_hash{$app_name}[0]<BR>Instances running: $qa01_hash{$app_name}[3]/$qa01_hash{$app_name}[4]<BR>Route: $qa01_hash{$app_name}[2]</td>";
-    print $fh "<td NOWRAP BGCOLOR=\"$qa02_hash{$app_name}[5]\">$qa02_app <BR>Version: 1.0.0.$qa02_hash{$app_name}[0]<BR>Instances running: $qa02_hash{$app_name}[3]/$qa02_hash{$app_name}[4]<BR>Route: $qa02_hash{$app_name}[2]</td></tr>";
+    print $fh "<td NOWRAP BGCOLOR=\"$qa02_hash{$app_name}[5]\">$qa02_app <BR>Version: 1.0.0.$qa02_hash{$app_name}[0]<BR>Instances running: $qa02_hash{$app_name}[3]/$qa02_hash{$app_name}[4]<BR>Route: $qa02_hash{$app_name}[2]</td>";
+    print $fh "<td NOWRAP BGCOLOR=\"$dd02_hash{$app_name}[5]\">$dd02_app <BR>Version: 1.0.0.$dd02_hash{$app_name}[0]<BR>Instances running: $dd02_hash{$app_name}[3]/$dd02_hash{$app_name}[4]<BR>Route: $dd02_hash{$app_name}[2]</td>";
+    print $fh "<td NOWRAP BGCOLOR=\"$dp02_hash{$app_name}[5]\">$dp02_app <BR>Version: 1.0.0.$dp02_hash{$app_name}[0]<BR>Instances running: $dp02_hash{$app_name}[3]/$dp02_hash{$app_name}[4]<BR>Route: $dp02_hash{$app_name}[2]</td></tr>";
 
     $app_count++;
 }

38530e7c20c8df6cad87a43d8a9b0c35b6b8562b
Kar, Prangya Parmita <Prangya.Kar@bhge.com>
2018-08-11 13:00:24 -0700

    add groovy

---
+++ jenkin-job-clone/abc.groovy
@@ -0,0 +1,17 @@
+#!groovy
+
+node('master') {
+    FILES_DIR = '/var/lib/jenkins/' 
+ 	#Home directory	/var/lib/jenkins
+    cleanWs()
+
+    def TMP_FILENAME = ".folder_list"
+    sh "ls ${FILES_DIR} > ${TMP_FILENAME}"
+    def filenames = readFile(TMP_FILENAME).split( "\\r?\\n" );
+    #sh "rm -f ${TMP_FILENAME}"
+
+    for (int i = 0; i < filenames.size(); i++) {
+        def filename = filenames[i]
+        echo "${filename}"
+    }
+}
